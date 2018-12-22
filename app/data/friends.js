334e14a9b62c79c9752c6b8cc775b2d20fb6abbd
Matt Milkovich <mattmilkovich@MacBook-Pro.local>
2018-12-22 00:17:56 -0600

    FriendFinerV1

---
+++ app/data/friends.js
@@ -0,0 +1,71 @@
+
+var friends = [
+    {
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+    },
+    {
+        "name":"Jerry",
+        "photo":"https://vignette.wikia.nocookie.net/characters/images/7/73/Jerry_561_b.jpg/revision/latest?cb=20151124041404",
+        "scores":[
+            2,
+            1,
+            4,
+            1,
+            5,
+            1,
+            1,
+            3,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Tom",
+        "photo":"http://pngimg.com/uploads/tom_and_jerry/tom_and_jerry_PNG44.png",
+        "scores":[
+            3,
+            4,
+            1,
+            4,
+            1,
+            1,
+            2,
+            5,
+            4,
+            5
+          ]
+      },
+      {
+        "name":"Tina",
+        "photo":"https://assets.vogue.com/photos/5891c8b097a3db337a24c8f7/master/pass/llamas-and-haircuts-prince-harry1.jpg",
+        "scores":[
+            4,
+            1,
+            4,
+            4,
+            4,
+            2,
+            3,
+            5,
+            3,
+            1
+          ]
+      },
+     
+      
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require("path");
+var mysql = require("mysql")
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "JawsDB"
+  });
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
